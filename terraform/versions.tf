9b45f8e9c9cb696eebf4530bfdf55ee0a7d9e85b
Julie08080808 <eilujchang9280@gmail.com>
2026-06-01 02:39:13 +0800

    Add required providers and version constraints

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,1 +1,25 @@
+terraform {
+  required_version = ">= 1.6.0"
 
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.27"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.13"
+    }
+
+    harbor = {
+      source  = "goharbor/harbor"
+      version = "~> 3.10"
+    }
+
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
