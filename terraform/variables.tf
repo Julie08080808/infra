3fac6ca89781397ae86bb3a0dffa5fa148d98583
Julie08080808 <eilujchang9280@gmail.com>
2026-06-01 02:39:47 +0800

    Define variables for Harbor and GitHub
    Added variables for Harbor registry and GitHub configuration.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,24 @@
+variable "harbor_url" {
+  description = "Harbor registry URL"
+  default     = "https://10.32.20.51:30443"
+}
 
+variable "harbor_admin_password" {
+  description = "Harbor admin password"
+  sensitive   = true
+}
+
+variable "github_token" {
+  description = "GitHub Personal Access Token"
+  sensitive   = true
+}
+
+variable "github_owner" {
+  description = "GitHub owner/organization"
+  default     = "Julie08080808"
+}
+
+variable "kubeconfig_path" {
+  description = "Path to kubeconfig file"
+  default     = "~/.kube/config"
+}
