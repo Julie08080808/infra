5aba38ed9eac885f57ba5e60393acb8a86a57f10
Julie08080808 <eilujchang9280@gmail.com>
2026-06-01 18:46:29 +0800

    Add outputs for robot account name and secret

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,8 @@
+output "robot_account_name" {
+  value = module.harbor.robot_account_name
+}
 
+output "robot_account_secret" {
+  value     = module.harbor.robot_account_secret
+  sensitive = true
+}
