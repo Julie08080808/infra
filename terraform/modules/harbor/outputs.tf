8ea72f4d31a9c93b9e4e3af2c165c35ac43b8af6
Julie08080808 <eilujchang9280@gmail.com>
2026-06-01 19:15:54 +0800

    Add output for robot account full name

--- terraform/modules/harbor/outputs.tf
+++ terraform/modules/harbor/outputs.tf
@@ -2,6 +2,10 @@ output "robot_account_name" {
   value = harbor_robot_account.arc_runner.name
 }
 
+output "robot_account_full_name" {
+  value = harbor_robot_account.arc_runner.full_name
+}
+
 output "robot_account_secret" {
   value     = harbor_robot_account.arc_runner.secret
   sensitive = true
